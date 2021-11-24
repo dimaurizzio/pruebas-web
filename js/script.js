bd73db3de38e2d588c6437c7afc6b678323ddf0f
cesar d m <cesardemaurizio1@gmail.com>
2021-11-24 12:45:31 -0300

    multiples avances en home

---
+++ js/script.js
@@ -0,0 +1,35 @@
+window.addEventListener('load', function(){
+    new Glider(document.querySelector('.glider'), {
+        slidesToScroll: 'auto',
+        slidesToShow: 1,
+        duration: 1,
+        draggable: true,
+        dragVelocity: 1,
+        arrows: {
+          prev: '.glider-prev',
+          next: '.glider-next'
+        },
+        responsive: [
+            {
+              // screens greater than >= 775px
+              breakpoint: 775,
+              settings: {
+                // Set to `auto` and provide item width to adjust to viewport
+                slidesToShow: 'auto',
+                slidesToScroll: 'auto',
+                itemWidth: 150,
+                duration: 1
+              }
+            },{
+              // screens greater than >= 1024px
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 'auto',
+                slidesToScroll: 'auto',
+                itemWidth: 150,
+                duration: 1
+              }
+            }
+          ]
+      });
+  });
